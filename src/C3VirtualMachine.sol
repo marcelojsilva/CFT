520f075e99a0b92e907895be3c45dde6077af056
marcelojsilva <marcelojosedasilva@gmail.com>
2024-07-30 15:49:10 -0300

    code review refactory

--- src/C3VirtualMachine.sol
+++ src/C3VirtualMachine.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
 
 import '@openzeppelin/contracts/utils/Counters.sol';
@@ -15,7 +14,7 @@ interface ERC20 {
  * @title C3VirtualMachine
  * @dev This contract manages the creation and pricing of virtual machines, utilizing ERC20 tokens for payment.
  */
-contract C3VirtualMachine is C3VirtualMachinePricing {
+contract C3VirtualMachine {
     using Counters for Counters.Counter;
 
     struct VirtualMachine {
@@ -28,20 +27,34 @@ contract C3VirtualMachine is C3VirtualMachinePricing {
 
     Counters.Counter private nextId;
     address public tokenAddress;
+    address public vmPricing;
     mapping(uint256 => VirtualMachine) public virtualMachines;
+    mapping(address => uint256[]) private ownerToVms; // Mapping from owner to their VMs
     mapping(address => uint256) public userCredits;
 
-    /// @notice Emitted when a new virtual machine is created
-    /// @param vmId The ID of the created virtual machine
-    /// @param vmOwner The owner of the created virtual machine
+    /// @notice Emitted when a virtual machine is created
+    /// @param vmId The unique ID of the virtual machine
+    /// @param vmOwner The owner of the virtual machine
     event VirtualMachineCreated(uint256 vmId, address indexed vmOwner);
+
+    /// @notice Emitted when tokens are deposited
+    /// @param user The user who deposited the tokens
+    /// @param amount The amount of tokens deposited
+    event TokensDeposited(address indexed user, uint256 amount);
+
+    /// @notice Emitted when tokens are withdrawn
+    /// @param user The user who withdrew the tokens
+    /// @param amount The amount of tokens withdrawn
+    event TokensWithdrawn(address indexed user, uint256 amount);
 
     /**
      * @dev Sets the ERC20 token address used for payments.
      * @param _tokenAddress The address of the ERC20 token contract.
+     * @param _vmPricing The address of the C3VirtualMachinePricing contract.
      */
-    constructor(address _tokenAddress) {
+    constructor(address _tokenAddress, address _vmPricing) {
         tokenAddress = _tokenAddress;
+        vmPricing = _vmPricing;
     }
 
     /**
@@ -49,29 +62,35 @@ contract C3VirtualMachine is C3VirtualMachinePricing {
      * @param vmType The type of the virtual machine to create
      * @param totalHoursToRun The total hours the virtual machine is supposed to run
      * @return vmId The ID of the created virtual machine
-     */
+    */
     function createVirtualMachine(uint256 vmType, uint256 totalHoursToRun) public returns (uint256) {
-        require(exists(vmType), "Virtual machine type does not exist");
-        require(!virtualMachineTypes[vmType].deprecated, "Virtual machine type is deprecated");
         require(totalHoursToRun > 0, "Total hours to run must be greater than 0");
 
-        uint256 pricePerHour = virtualMachineTypes[vmType].pricePerHour;
-        uint256 creditsToConsume = pricePerHour * totalHoursToRun;
-        require(userCredits[msg.sender] >= creditsToConsume, "Insufficient balance to start virtual machine");
+        C3VirtualMachinePricing pricingContract = C3VirtualMachinePricing(vmPricing);
+        require(pricingContract.idExists(vmType), "Virtual machine type does not exist");
+
+        // Access only the necessary fields individually
+        (, uint256 _pricePerHour, bool _deprecated) = pricingContract.virtualMachineTypes(vmType);
+        require(!_deprecated, "Virtual machine type is deprecated");
+
+        uint256 creditsToConsume = _pricePerHour * totalHoursToRun;
+        address sender = msg.sender;
+        require(userCredits[sender] >= creditsToConsume, "Insufficient balance to start virtual machine");
 
         nextId.increment();
         uint256 vmId = nextId.current();
         virtualMachines[vmId] = VirtualMachine({
-            vmOwner: msg.sender,
+            vmOwner: sender,
             vmType: vmType,
             startTime: block.timestamp,
             totalHoursToRun: totalHoursToRun,
-            pricePerHour: pricePerHour
+            pricePerHour: _pricePerHour
         });
 
-        userCredits[msg.sender] -= creditsToConsume;
+        userCredits[sender] -= creditsToConsume;
+        ownerToVms[sender].push(vmId); // Update owner to VMs mapping
 
-        emit VirtualMachineCreated(vmId, msg.sender);
+        emit VirtualMachineCreated(vmId, sender);
 
         return vmId;
     }
@@ -81,23 +100,7 @@ contract C3VirtualMachine is C3VirtualMachinePricing {
      * @return myVirtualMachines An array of virtual machine IDs owned by the caller
      */
     function getMyVirtualMachines() external view returns (uint256[] memory) {
-        uint256 count = 0;
-        for (uint256 i = 1; i <= nextId.current(); i++) {
-            if (virtualMachines[i].vmOwner == msg.sender) {
-                count++;
-            }
-        }
-
-        uint256[] memory myVirtualMachines = new uint256[](count);
-        count = 0;
-        for (uint256 i = 1; i <= nextId.current(); i++) {
-            if (virtualMachines[i].vmOwner == msg.sender) {
-                myVirtualMachines[count] = i;
-                count++;
-            }
-        }
-
-        return myVirtualMachines;
+        return ownerToVms[msg.sender];
     }
 
     /**
@@ -109,6 +112,7 @@ contract C3VirtualMachine is C3VirtualMachinePricing {
         require(token.transferFrom(msg.sender, address(this), amount), "Token transfer failed");
 
         userCredits[msg.sender] += amount;
+        emit TokensDeposited(msg.sender, amount); // Emit event for deposit
     }
 
     /**
@@ -121,13 +125,6 @@ contract C3VirtualMachine is C3VirtualMachinePricing {
         require(token.transfer(msg.sender, amount), "Token transfer failed");
 
         userCredits[msg.sender] -= amount;
-    }
-
-    /**
-     * @notice Retrieves the balance of the caller's credits
-     * @return The balance of the caller's credits
-     */
-    function myCreditsBalance() external view returns (uint256) {
-        return userCredits[msg.sender];
+        emit TokensWithdrawn(msg.sender, amount); // Emit event for withdrawal
     }
 }
--- src/C3VirtualMachinePricing.sol
+++ src/C3VirtualMachinePricing.sol
@@ -13,110 +13,90 @@ contract C3VirtualMachinePricing {
         bool deprecated; // Flag to indicate if the virtual machine model is deprecated
     }
 
-    VirtualMachineType[] public virtualMachineTypes;
+    mapping(uint256 => VirtualMachineType) public virtualMachineTypes;
+    mapping(uint256 => bool) public idExists; // Changed to public
 
     /// @notice Emitted when a new virtual machine type is created
+    /// @param id The unique ID of the virtual machine model
     /// @param modelName The name of the virtual machine model
     /// @param pricePerHour The price per hour for the virtual machine model
-    event TypeCreated(string modelName, uint256 pricePerHour);
+    event TypeCreated(uint256 indexed id, string modelName, uint256 pricePerHour);
 
     /// @notice Emitted when a virtual machine model name is updated
+    /// @param id The unique ID of the virtual machine model
     /// @param modelName The updated model name
-    event ModelNameUpdated(uint256 indexed index, string modelName);
+    event ModelNameUpdated(uint256 indexed id, string modelName);
 
     /// @notice Emitted when the price per hour is updated
+    /// @param id The unique ID of the virtual machine model
     /// @param pricePerHour The updated price per hour
-    event PricePerHourUpdated(uint256 indexed index, uint256 pricePerHour);
+    event PricePerHourUpdated(uint256 indexed id, uint256 pricePerHour);
 
     /// @notice Emitted when the deprecated status is toggled
+    /// @param id The unique ID of the virtual machine model
     /// @param deprecated The new deprecated status
-    event DeprecatedStatusToggled(uint256 indexed index, bool deprecated);
+    event DeprecatedStatusToggled(uint256 indexed id, bool deprecated);
+
+    modifier typeExists(uint256 id) {
+        require(idExists[id], "Virtual machine type does not exist");
+        _;
+    }
 
     /**
      * @notice Creates a new virtual machine type
+     * @param id The unique ID of the virtual machine model
      * @param _modelName The name of the virtual machine model
      * @param _pricePerHour The price per hour for the virtual machine model
      */
-    function createVirtualMachineType(string calldata _modelName, uint256 _pricePerHour) external {
+    function createVirtualMachineType(uint256 id, string calldata _modelName, uint256 _pricePerHour) external {
         require(bytes(_modelName).length > 0, "Model name cannot be empty");
         require(_pricePerHour > 0, "Price per hour must be greater than 0");
+        require(!idExists[id], "ID already exists");
 
-        virtualMachineTypes.push(VirtualMachineType({
+        virtualMachineTypes[id] = VirtualMachineType({
             modelName: _modelName,
             pricePerHour: _pricePerHour,
             deprecated: false
-        }));
+        });
 
-        emit TypeCreated(_modelName, _pricePerHour);
-    }
+        idExists[id] = true;
 
-    /**
-     * @notice Checks if a virtual machine type exists at a given index
-     * @param _index The index to check
-     * @return bool True if the virtual machine type exists, false otherwise
-     */
-    function exists(uint256 _index) public view returns (bool) {
-        return _index < virtualMachineTypes.length;
-    }
-
-    /**
-     * @notice Retrieves the details of a virtual machine type
-     * @param _index The index of the virtual machine type to retrieve
-     * @return modelName The name of the virtual machine model
-     * @return pricePerHour The price per hour for the virtual machine model
-     * @return deprecated The deprecated status of the virtual machine model
-     */
-    function get(uint256 _index)
-        public
-        view
-        returns (string memory modelName, uint256 pricePerHour, bool deprecated)
-    {
-        require(exists(_index), "Virtual machine type does not exist");
-
-        VirtualMachineType storage virtualMachineType = virtualMachineTypes[_index];
-        return (virtualMachineType.modelName, virtualMachineType.pricePerHour, virtualMachineType.deprecated);
+        emit TypeCreated(id, _modelName, _pricePerHour);
     }
 
     /**
      * @notice Updates the model name of a virtual machine type
-     * @param _index The index of the virtual machine type to update
+     * @param id The ID of the virtual machine type to update
      * @param _modelName The new model name
      */
-    function updateVirtualMachineModelName(uint256 _index, string calldata _modelName) external {
-        require(exists(_index), "Virtual machine type does not exist");
+    function updateVirtualMachineModelName(uint256 id, string calldata _modelName) external typeExists(id) {
         require(bytes(_modelName).length > 0, "Model name cannot be empty");
 
-        VirtualMachineType storage virtualMachineType = virtualMachineTypes[_index];
-        virtualMachineType.modelName = _modelName;
+        virtualMachineTypes[id].modelName = _modelName;
 
-        emit ModelNameUpdated(_index, _modelName);
+        emit ModelNameUpdated(id, _modelName);
     }
 
     /**
      * @notice Updates the price per hour of a virtual machine type
-     * @param _index The index of the virtual machine type to update
+     * @param id The ID of the virtual machine type to update
      * @param _pricePerHour The new price per hour
      */
-    function updateVirtualMachinePricePerHour(uint256 _index, uint256 _pricePerHour) external {
-        require(exists(_index), "Virtual machine type does not exist");
+    function updateVirtualMachinePricePerHour(uint256 id, uint256 _pricePerHour) external typeExists(id) {
         require(_pricePerHour > 0, "Price per hour must be greater than 0");
 
-        VirtualMachineType storage virtualMachineType = virtualMachineTypes[_index];
-        virtualMachineType.pricePerHour = _pricePerHour;
+        virtualMachineTypes[id].pricePerHour = _pricePerHour;
 
-        emit PricePerHourUpdated(_index, _pricePerHour);
+        emit PricePerHourUpdated(id, _pricePerHour);
     }
 
     /**
      * @notice Toggles the deprecated status of a virtual machine type
-     * @param _index The index of the virtual machine type to update
+     * @param id The ID of the virtual machine type to update
      */
-    function toggleVirtualMachineDeprecatedStatus(uint256 _index) external {
-        require(exists(_index), "Virtual machine type does not exist");
+    function toggleVirtualMachineDeprecatedStatus(uint256 id) external typeExists(id) {
+        virtualMachineTypes[id].deprecated = !virtualMachineTypes[id].deprecated;
 
-        VirtualMachineType storage virtualMachineType = virtualMachineTypes[_index];
-        virtualMachineType.deprecated = !virtualMachineType.deprecated;
-
-        emit DeprecatedStatusToggled(_index, virtualMachineType.deprecated);
+        emit DeprecatedStatusToggled(id, virtualMachineTypes[id].deprecated);
     }
 }
